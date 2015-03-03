8ac56453a5d4afb104232c9415b98660ad2367b1
Daniel Graham <oloriun@hotmail.com>
2015-03-03 18:14:35 +0000

    heroku push

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,15 +19,14 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+
+  
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -76,7 +75,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.serve_static_assets = true
-  config.action_dispatch.x_sendfile_header = ‘X-Accel-Redirect’
-  config.assets.compile = true
+
+
 end
