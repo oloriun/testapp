ce7a8321c74c1ff105b69c19674552a1f0598378
Daniel Graham <oloriun@hotmail.com>
2015-03-11 20:14:21 +0000

    added colour and condition fields to product db and pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309144608) do
+ActiveRecord::Schema.define(version: 20150311200508) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20150309144608) do
     t.string   "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "colour"
+    t.string   "condition"
   end
 
 end
